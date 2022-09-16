366a2728f713d8245b4c3b3eabb268e9cedf795e
zelalem10 <zelalembe10@gmail.com>
2022-09-16 08:28:49 -0500

    dependecies added

---
+++ Spring-security/src/main/java/miu/waa/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package miu.waa.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
